10f000f59c98d7c2146c4c1ae0250687d94f886e
921starfish <yuya.921.hoshino@gmail.com>
2015-06-06 23:54:24 +0900

    no message

--- HighBridge/View/Pages/StartPage.xaml.cs
+++ HighBridge/View/Pages/StartPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HighBridge.ViewModel;
 
 namespace HighBridge.View.Pages
 {
@@ -23,6 +24,7 @@ namespace HighBridge.View.Pages
         public StartPage()
         {
             InitializeComponent();
+            DataContext = new StartPageViewModel();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
