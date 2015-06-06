60496ac0e4d11fd80fc9e61984b00bd2635b1f6b
921starfish <yuya.921.hoshino@gmail.com>
2015-06-06 22:37:56 +0900

    Update LoginPage.xaml

--- HighBridge/View/Pages/LoginPage.xaml.cs
+++ HighBridge/View/Pages/LoginPage.xaml.cs
@@ -1,5 +1,13 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Windows.Media;
+using System.Windows.Resources;
 using HighBridge.ViewModel;
+
 
 namespace HighBridge.View.Pages
 {
@@ -13,5 +21,21 @@ namespace HighBridge.View.Pages
             InitializeComponent();
             DataContext = new LoginPageViewModel();
         }
+
+        private void FaceMe_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            FaceMe.Visibility = Visibility.Collapsed;
+            Loading.Visibility = Visibility.Visible;
+        }
+        private void WindowsFormsHost_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowsFormsHost host = (WindowsFormsHost)sender;
+            PictureBox picBox = (PictureBox)host.Child;
+            StreamResourceInfo streamInfo =
+                System.Windows.Application.GetResourceStream(
+                    new Uri("Assets/loading.gif", UriKind.Relative));
+            picBox.Image = (Bitmap)Bitmap.FromStream(streamInfo.Stream);
+        }
+
     }
 }
