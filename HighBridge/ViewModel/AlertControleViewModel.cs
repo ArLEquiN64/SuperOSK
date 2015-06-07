8225da6b96e042befee7487afcdf09391fb90d3c
MrYD <azelf.trickroom@gmail.com>
2015-06-07 10:16:53 +0900

    no message

---
+++ HighBridge/ViewModel/AlertControleViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using HighBridge.Common.Commands;
+using HighBridge.Common.Util;
+
+namespace HighBridge.ViewModel
+{
+    class AlertControleViewModel:ViewModelBase
+    {
+        private bool _visibility;
+
+        public AlertControleViewModel(string str,Action act)
+        {
+            Text = str;
+            Visibility = false;
+            Command=new AlwaysExecutableDelegateCommand(o =>
+            {
+                act();
+            });
+        }
+
+        public bool Visibility
+        {
+            get { return _visibility; }
+            set
+            {
+                _visibility = value;
+                OnpropertyChanged();
+            }
+        }
+
+        public string Text { get; set; }
+
+        public AlwaysExecutableDelegateCommand Command { get; set; }
+    }
+}
--- HighBridge/ViewModel/LoginPageViewModel.cs
+++ HighBridge/ViewModel/LoginPageViewModel.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using HighBridge.Common.Util;
+using HighBridge.View;
+using HighBridge.View.Pages;
 
 namespace HighBridge.ViewModel
 {
-    class LoginPageViewModel:ViewModelBase
+    class LoginPageViewModel : ViewModelBase
     {
+        private Timer _timer;
+        private readonly AlertControleViewModel _controleData;
+
         public LoginPageViewModel()
         {
+            UserName = "wang ?";
+            _controleData = new AlertControleViewModel(UserName, (() =>
+                  MainWindow.Instance.Dispatcher.Invoke(() =>
+                  {
+                      MainWindow.Instance.NavigationService.Navigate(new MainPage());
+                  })
+              ));
+            _timer = new Timer((state =>
+            {
+                _timer.Dispose();
+                ControleData.Visibility = true;
+                OnpropertyChanged();
+            }), null, 1000, 1000);
+        }
+
+        private string UserName { get; set; }
+
+        public AlertControleViewModel ControleData
+        {
+            get { return _controleData; }
         }
     }
 }
