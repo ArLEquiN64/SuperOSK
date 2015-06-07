6738ad279dcbc827e5e9b47cf2869d4bf131c82a
ArLEquiN64 <ARLEQUIN.prx@gmail.com>
2015-06-07 12:45:06 +0900

    add login

--- HighBridge/Model/AccountManager.cs
+++ HighBridge/Model/AccountManager.cs
@@ -57,6 +57,46 @@ namespace HighBridge.Model
             sr.Close();
         }
 
+		public static void logIn()
+		{
+			//文字コードを指定する
+			System.Text.Encoding enc = System.Text.Encoding.GetEncoding("shift_jis");
+
+			//POST送信する文字列を作成
+			string postData =
+				"id=" + MyData.FaceId;
+
+			//バイト型配列に変換
+			byte[] postDataBytes = System.Text.Encoding.ASCII.GetBytes(postData);
+
+			//WebRequestの作成
+			System.Net.WebRequest req =
+				System.Net.WebRequest.Create("http://VirtualOSK.cloudapp.net/login");
+			//メソッドにPOSTを指定
+			req.Method = "GET";
+			//ContentTypeを"application/x-www-form-urlencoded"にする
+			req.ContentType = "application/x-www-form-urlencoded";
+			//POST送信するデータの長さを指定
+			req.ContentLength = postDataBytes.Length;
+
+			//データをPOST送信するためのStreamを取得
+			System.IO.Stream reqStream = req.GetRequestStream();
+			//送信するデータを書き込む
+			reqStream.Write(postDataBytes, 0, postDataBytes.Length);
+			reqStream.Close();
+
+			//サーバーからの応答を受信するためのWebResponseを取得
+			System.Net.WebResponse res = req.GetResponse();
+			//応答データを受信するためのStreamを取得
+			System.IO.Stream resStream = res.GetResponseStream();
+			//受信して表示
+			System.IO.StreamReader sr = new System.IO.StreamReader(resStream, enc);
+			var str = sr.ReadToEnd();
+			//閉じる
+			sr.Close();
+			_sessionId = str;
+		}
+
 		public static UserData GetUserInfo(string faceid)
 		{
 			//文字コードを指定する
