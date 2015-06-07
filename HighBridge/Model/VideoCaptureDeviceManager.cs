6680ec8936fb77c059c318cf7116f05a6646589c
MrYD <azelf.trickroom@gmail.com>
2015-06-07 13:54:31 +0900

    no message

--- HighBridge/Model/VideoCaptureDeviceManager.cs
+++ HighBridge/Model/VideoCaptureDeviceManager.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using AForge.Video;
 using AForge.Video.DirectShow;
+using HighBridge.Common.Util;
 
 namespace HighBridge.Model
 {
@@ -13,6 +16,8 @@ namespace HighBridge.Model
     {
         public static VideoCaptureDevice Device { get; set; }
         public static event NewFrameEventHandler NewFrameGot = delegate { };
+        private static Timer _timer;
+        public static BitmapFrame Frame { get; set; }
         public static Bitmap Bitmap { get; set; }
 
         public static void Connect(string deviceName)
@@ -20,13 +25,14 @@ namespace HighBridge.Model
             if(deviceName==null)return;
             Device = new VideoCaptureDevice(deviceName);
             Device.Start();
-            NewFrameGot += CamDeviceCtrlNewFrameGot;
-            Device.NewFrame += NewFrameGot;
+            Device.NewFrame += CamDeviceCtrlNewFrameGot;
         }
 
         private static void CamDeviceCtrlNewFrameGot(object sender, NewFrameEventArgs eventArgs)
         {
-            Bitmap = eventArgs.Frame;
+            Frame = BitmapToBitmapFrame.Convert(eventArgs.Frame);
+            Bitmap = new Bitmap(eventArgs.Frame);
+            NewFrameGot(sender, eventArgs);
         }
 
         public static void DisConnect()
--- HighBridge/ViewModel/MainPageViewModel.cs
+++ HighBridge/ViewModel/MainPageViewModel.cs
@@ -3,21 +3,51 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Microsoft.Win32;
 using HighBridge.Common.Util;
+using HighBridge.Model;
 
 namespace HighBridge.ViewModel
 {
     class MainPageViewModel:ViewModelBase
     {
+        private BitmapFrame _imageSource;
+        private Timer _timer;
+
         public MainPageViewModel()
         {
-            
+            TwitterId = AccountManager.MyData.TwitterId;
+            Comment = AccountManager.MyData.Comment;
+            //_timer=new Timer((state =>
+            //{
+            //    if (VideoCaptureDeviceManager.Bitmap==null)return;
+            //    ImageSource = BitmapToBitmapFrame.Convert(VideoCaptureDeviceManager.Bitmap);
+            //}),null,100,100);
+
+            VideoCaptureDeviceManager.NewFrameGot += (s, e) =>
+            {
+                if (VideoCaptureDeviceManager.Bitmap == null) return;
+                ImageSource = BitmapToBitmapFrame.Convert(VideoCaptureDeviceManager.Bitmap);
+            };
         }
-        public Bitmap ImageSource { get; set; }
+
+        public BitmapFrame ImageSource
+        {
+            get { return _imageSource; }
+            set
+            {
+                _imageSource = value; 
+                OnpropertyChanged("ImageSource");
+            }
+        }
+
+        public string TwitterId { get; set; }
+        public string Comment { get; set; }
+
 
     }
 }
